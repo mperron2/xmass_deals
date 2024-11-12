9120f5bf0355d8ace526b0b3474181ca6eafce04
mperron2 <mperron2@academic.rrc.ca>
2024-11-12 14:25:59 -0600

    Active admin and active storage added

--- db/migrate/20241112194344_create_orders.rb
+++ db/migrate/20241112194344_create_orders.rb
@@ -2,7 +2,6 @@ class CreateOrders < ActiveRecord::Migration[7.2]
   def change
     create_table :orders do |t|
       t.integer :cost
-      t.datetime :created_at
       t.string :status
       t.references :customer, null: false, foreign_key: true
 
