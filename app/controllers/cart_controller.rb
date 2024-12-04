d2d61d9d52e83e32bf34787baaf8237e44f81fe4
mperron2 <mperron2@academic.rrc.ca>
2024-12-03 20:44:54 -0600

    feat/numbers of items in cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,24 +2,37 @@ class CartController < ApplicationController
   before_action :get_product, only: %i[create destroy]
 
   def create
-    unless session[:cart].include?(@product.id)
-      session[:cart] << @product.id
-    end
+    product_id = @product.id.to_s
+    session[:cart][product_id] ||= 0
+    session[:cart][product_id] += 1
 
     redirect_to root_path
   end
 
   def destroy
-    if session[:cart].include?(@product.id)
-      session[:cart].delete(@product.id)
-    end
+    product_id = @product.id.to_s
+    session[:cart].delete(product_id)
+
+    redirect_to root_path
+  end
+
+  def update_quantity
+    product_id = params[:id]
+    quantity = params[:quantity].to_i
+    session[:cart][product_id] = quantity if quantity > 0
 
     redirect_to root_path
   end
 
   private
+
+  # Define the get_product method
   def get_product
-    product_id = params[:id].to_i
-    @product = Product.find(product_id)
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+
+    unless @product
+      redirect_to root_path, alert: "Product not found"
+    end
   end
 end
