e9d216068fc91b04f5b1c6cb1ef5307addf30141
mperron2 <mperron2@academic.rrc.ca>
2024-12-06 13:57:26 -0600

    Added tax in orders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def get_tax_rate(province)
+    {
+      "Alberta" => 0.05,
+      "British Columbia" => 0.12,
+      "Manitoba" => 0.12,
+      "New Brunswick" => 0.15,
+      "Newfoundland and Labrador" => 0.15,
+      "Nova Scotia" => 0.15,
+      "Ontario" => 0.13,
+      "Prince Edward Island" => 0.15,
+      "Quebec" => 0.14975,
+      "Saskatchewan" => 0.06
+    }[province] || 0.15
+  end
 end
